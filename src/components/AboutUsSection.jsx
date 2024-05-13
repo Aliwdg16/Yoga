4fd76c965d56dd537bd844fe3c562c1ecc6adf3e
tdo leipzig <tdo.leipzig@gmail.com>
2024-05-13 14:07:16 +0200

    modify pricing & about us sections

--- src/components/AboutUsSection.jsx
+++ src/components/AboutUsSection.jsx
@@ -1,12 +1,46 @@
 function AboutUsSection() {
 
 	return (
-		<div className="bg-green-200">
+		<div className="bg-[#b5f0db] relative overflow-hidden">
+			<img src="src/assets/lotus_section_3.png" alt="background" className="absolute w-full h-full object-contain -bottom-[5rem] z-0 object-bottom"/>
 
-			<div className="section container mx-auto py-16 px-8 text-slate-800">
-				About Us Section
+			<div className="relative z-10 mx-auto max-w-[400px] sm:max-w-[800px] lg:max-w-[1200px] py-12 px-8 md:py-16 xl:py-20 text-[#473E59] text-center md:text-lg lg:text-xl">
+				<h2 className="uppercase text-5xl font-bold">About Us</h2>
+				<p className="py-4 sm:py-8 max-w-[700px] mx-auto">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+				<div className="grid gap-4 lg:gap-8 xl:gap-12 grid-cols-1 sm:grid-cols-3">
+
+					<div className="rounded-lg overflow-hidden">
+						<div className="aspect-square overflow-hidden">
+							<img src="src/assets/image_about_us_1.jpg" alt="yoga practice 1" className="object-cover w-full h-full"/>
+						</div>
+						<div className="bg-white p-4 pb-6">
+							<h3 className="text-xl font-medium uppercase">Weekly Yoga</h3>
+							<p className="pt-3">Lorem ipsum dolor sit amet, consetetur sadipscing elit.</p>
+						</div>
+					</div>
+
+					<div className="rounded-lg overflow-hidden">
+						<div className="aspect-square overflow-hidden">
+							<img src="src/assets/image_about_us_2.jpg" alt="yoga practice 2" className="object-cover w-full h-full"/>
+						</div>
+						<div className="bg-white p-4 pb-6">
+							<h3 className="text-xl font-medium uppercase">Monthly Yoga</h3>
+							<p className="pt-3">Lorem ipsum dolor sit amet, consetetur sadipscing elit.</p>
+						</div>
+					</div>
+
+					<div className="rounded-lg overflow-hidden">
+						<div className="aspect-square overflow-hidden">
+							<img src="src/assets/image_about_us_3.jpg" alt="yoga practice 3" className="object-cover w-full h-full"/>
+						</div>
+						<div className="bg-white p-4 pb-6">
+							<h3 className="text-xl font-medium uppercase">Monthly Yoga</h3>
+							<p className="pt-3">Lorem ipsum dolor sit amet, consetetur sadipscing elit.</p>
+						</div>
+					</div>
+
+				</div>
 			</div>
-
 		</div>
 	)
 }
--- src/components/PricingSection.jsx
+++ src/components/PricingSection.jsx
@@ -1,10 +1,43 @@
 function PricingSection() {
 
 	return (
-		<div className="bg-green-200">
+		<div className="bg-[#b5f0db]">
 
-			<div className="section container mx-auto py-16 px-8 text-slate-800">
-				Pricing Section
+			<div className="bg-[#b5f0db] relative overflow-hidden">
+
+				<div className="relative z-10 mx-auto max-w-[400px] sm:max-w-[800px] lg:max-w-[1200px] py-12 px-8 md:py-16 xl:py-20 text-[#473E59] text-center md:text-lg lg:text-xl">
+					<h2 className="uppercase text-5xl font-bold">Pricing</h2>
+					<p className="py-4 sm:py-8 max-w-[700px] mx-auto">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+
+					<div className="grid gap-6 lg:gap-8 xl:gap-12 grid-cols-1 sm:grid-cols-3">
+
+
+						<div className="rounded-lg overflow-hidden bg-white px-4 py-6">
+							<h3 className="text-2xl font-bold">Bronze</h3>
+							<img src="src/assets/medal_icon.png" alt="medal" className="mx-auto py-4"/>
+							<span className="text-2xl font-bold">$105.00</span>
+							<p className="py-4">Lorem ipsum dolor sit amet, consetetur sadipscing elit.</p>
+							<button>Order now</button>
+						</div>
+
+						<div className="rounded-lg overflow-hidden bg-white px-4 py-6">
+							<h3 className="text-2xl font-bold">Gold</h3>
+							<img src="src/assets/medal_icon.png" alt="medal" className="mx-auto py-4"/>
+							<span className="text-2xl font-bold">$105.00</span>
+							<p className="py-4">Lorem ipsum dolor sit amet, consetetur sadipscing elit.</p>
+							<button>Order now</button>
+						</div>
+
+						<div className="rounded-lg overflow-hidden bg-white px-4 py-6">
+							<h3 className="text-2xl font-bold">Platinum</h3>
+							<img src="src/assets/medal_icon.png" alt="medal" className="mx-auto py-4"/>
+							<span className="text-2xl font-bold">$105.00</span>
+							<p className="py-4">Lorem ipsum dolor sit amet, consetetur sadipscing elit.</p>
+							<button>Order now</button>
+						</div>
+
+					</div>
+				</div>
 			</div>
 
 		</div>
